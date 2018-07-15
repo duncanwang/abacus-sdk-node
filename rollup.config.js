d03e50887ad5ed9b9ffa1a8464ff446e991eb6d3
Ian Macalinao <me@ian.pw>
2018-07-15 12:43:29 -0700

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "./src/index.ts",
+  output: {
+    file: "./dist/bundle.js",
+    format: "iife",
+    name: "monoblockPanel"
+  },
+  plugins: [typescript()]
+};
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import SDK from "@abacusprotocol/sdk-js";
+const SHA3 = require("sha3");
+
+const sha3 = (input: string): string => {
+  const d = new SHA3.SHA3Hash();
+  d.update(input);
+  return d.digest("hex");
+};
+
+class ServerSDK extends SDK {
+  _apiSecret: string;
+
+  constructor(params: {
+    apiKey: string;
+    apiSecret: string;
+    applicationId?: string;
+    apiURL?: string;
+  }) {
+    super({
+      apiURL: params.apiURL,
+      applicationId: params.applicationId,
+      authToken: params.apiKey
+    });
+    this._apiSecret = params.apiSecret;
+  }
+
+  async _sendRequest(path: string, mergeOpts: { [key: string]: any } = {}) {
+    const method = mergeOpts.method || "GET";
+    const url = `${this._apiBase}${path}`;
+    const body = mergeOpts.body || "";
+    const signature = sha3(`${method} ${url}${body}`.trim());
+
+    const res = await fetch(this._apiBase + url, {
+      headers: {
+        "content-type": "application/json",
+        Authorization: "Token " + this._authToken,
+        "X-Signature": signature
+      },
+      ...mergeOpts
+    });
+    return await res.json();
+  }
+}
+
+export default ServerSDK;
